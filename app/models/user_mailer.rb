a12eb2ec437d1392e7ad445a6b770e644de772f0
jbeck <jbeck@461604b8-cbb9-4c6a-b984-f4fa5d6bc773>
2009-04-19 22:48:27 +0000

    lots of styling on the resume and on the blog.
    git-svn-id: svn+ssh://svn.beckproduct.com/svnroot/justinbeck/trunk@23 461604b8-cbb9-4c6a-b984-f4fa5d6bc773

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -2,7 +2,6 @@ class UserMailer < ActionMailer::Base
   def signup_notification(user)
     setup_email(user)
     @subject    += 'Please activate your new account'
-  
     @body[:url]  = "http://www.justinbeck.com/activate/#{user.activation_code}"
   
   end
@@ -11,6 +10,13 @@ class UserMailer < ActionMailer::Base
     setup_email(user)
     @subject    += 'Your account has been activated!'
     @body[:url]  = "http://www.justinbeck.com/"
+  end
+
+  def comment(subject, username)
+    setup_email(User.find(1))
+    @subject    += 'New comment on the blog!'
+    @body       += username + ' commented on ' + subject
+    logger.info "Email sent to " + @recipients
   end
   
   protected
--- vendor/plugins/blog/app/controllers/comments_controller.rb
+++ vendor/plugins/blog/app/controllers/comments_controller.rb
@@ -16,6 +16,7 @@ class CommentsController < ApplicationController
 
     if @comment = @post.comments.create(params[:comment])
       flash[:notice] = "Comment Created"
+      UserMailer.deliver_comment(@post.subject, @comment.username)
       redirect_to :controller => 'posts', :action => 'index'
     else
       flash[:error] = "Comment Not Created"
