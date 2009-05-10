a3f713dc12b84f80d182437d4ff324120feebd18
jbeck <jbeck@461604b8-cbb9-4c6a-b984-f4fa5d6bc773>
2009-05-10 21:18:46 +0000

    Added ordering of comments as they come back.
    git-svn-id: svn+ssh://svn.beckproduct.com/svnroot/justinbeck/trunk@60 461604b8-cbb9-4c6a-b984-f4fa5d6bc773

--- vendor/plugins/blog/app/models/post.rb
+++ vendor/plugins/blog/app/models/post.rb
@@ -3,6 +3,13 @@
 # Date: Apr 6, 2009
 # Time: 4:40:22 PM
 class Post < ActiveRecord::Base
-    has_many :comments
+    has_many :comments, :order => 'created_at DESC'
     belongs_to :user
+
+    # TODO: Figure out what the hell this does
+#    def comment_count; self.comments.count; end
+#
+#    def to_xml(opts={})
+#        super(opts.merge(:include => [:posts], :methods => [:comment_count]))
+#    end
 end
