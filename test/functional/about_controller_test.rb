25e13e3a0c6d4bca2c21c610dac3d48f089fd151
jbeck <jbeck@461604b8-cbb9-4c6a-b984-f4fa5d6bc773>
2009-05-29 12:29:41 +0000

    Started to move towards WebRAT testing.
    git-svn-id: svn+ssh://svn.beckproduct.com/svnroot/justinbeck/trunk@68 461604b8-cbb9-4c6a-b984-f4fa5d6bc773

--- test/functional/about_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require File.dirname(__FILE__) + '/../test_helper'
-require 'about_controller'
-
-class AboutControllerTest < ActionController::TestCase
-  def test_index
-    get :index
-    assert :success
-  end
-end
