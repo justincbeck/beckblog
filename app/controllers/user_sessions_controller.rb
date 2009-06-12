77ff347f54a88fad9b244e800ab79362ed4584ec
jbeck <jbeck@461604b8-cbb9-4c6a-b984-f4fa5d6bc773>
2009-06-12 04:12:52 +0000

    Made minor changes to layouts.
    git-svn-id: svn+ssh://svn.beckproduct.com/svnroot/justinbeck/trunk@92 461604b8-cbb9-4c6a-b984-f4fa5d6bc773

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -13,7 +13,7 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
       flash[:notice] = "Login successful!"
-      redirect_back_or_default account_url
+      redirect_to '/'
     else
       render :action => :new
     end
@@ -22,7 +22,7 @@ class UserSessionsController < ApplicationController
   def destroy
     current_user_session.destroy
     flash[:notice] = "Logout successful!"
-    redirect_back_or_default new_user_session_url
+    redirect_to '/'
   end
 
   private
