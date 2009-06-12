2515423e49a09a1b9d860d90e68f6d6debdef5e0
jbeck <jbeck@461604b8-cbb9-4c6a-b984-f4fa5d6bc773>
2009-06-12 03:14:58 +0000

    Started adding AuthLogic stuff.
    git-svn-id: svn+ssh://svn.beckproduct.com/svnroot/justinbeck/trunk@86 461604b8-cbb9-4c6a-b984-f4fa5d6bc773

--- db/migrate/20090408123100_create_users.rb
+++ db/migrate/20090408123100_create_users.rb
@@ -1,22 +1,25 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table "users", :force => true do |t|
-      t.column :login,                     :string, :limit => 40
-      t.column :name,                      :string, :limit => 100, :default => '', :null => true
-      t.column :email,                     :string, :limit => 100
-      t.column :crypted_password,          :string, :limit => 40
-      t.column :salt,                      :string, :limit => 40
-      t.column :created_at,                :datetime
-      t.column :updated_at,                :datetime
-      t.column :remember_token,            :string, :limit => 40
-      t.column :remember_token_expires_at, :datetime
-      t.column :activation_code,           :string, :limit => 40
-      t.column :activated_at,              :datetime
-      t.column :state,                     :string, :null => :no, :default => 'passive'
-      t.column :deleted_at,                :datetime
+      t.string    :login,               :null => false                # optional, you can use email instead, or both
+      t.string    :email,               :null => false                # optional, you can use login instead, or both
+      t.string    :crypted_password,    :null => false                # optional, see below
+      t.string    :password_salt,       :null => false                # optional, but highly recommended
+      t.string    :persistence_token,   :null => false                # required
+      t.string    :single_access_token, :null => false                # optional, see Authlogic::Session::Params
+      t.string    :perishable_token,    :null => false                # optional, see Authlogic::Session::Perishability
+
+      # Magic columns, just like ActiveRecord's created_at and updated_at. These are automatically maintained by Authlogic if they are present.
+      t.integer   :login_count,         :null => false, :default => 0 # optional, see Authlogic::Session::MagicColumns
+      t.integer   :failed_login_count,  :null => false, :default => 0 # optional, see Authlogic::Session::MagicColumns
+      t.datetime  :last_request_at                                    # optional, see Authlogic::Session::MagicColumns
+      t.datetime  :current_login_at                                   # optional, see Authlogic::Session::MagicColumns
+      t.datetime  :last_login_at                                      # optional, see Authlogic::Session::MagicColumns
+      t.string    :current_login_ip                                   # optional, see Authlogic::Session::MagicColumns
+      t.string    :last_login_ip                                      # optional, see Authlogic::Session::MagicColumns
     end
     add_index :users, :login, :unique => true
-    execute "INSERT INTO `users` VALUES (1,'justin','','justinbeck@mac.com','2de89174530a7a02d92cc6b17a55891d9a83dac2','971e352bfe98379520ecdf94fbed358b91be7e0e','2009-04-08 12:44:41','2009-04-08 12:45:50',NULL,NULL,NULL,'2009-04-08 12:45:50','active',NULL);"
+    #execute "INSERT INTO `users` VALUES (1,'justin','','justinbeck@mac.com','2de89174530a7a02d92cc6b17a55891d9a83dac2','971e352bfe98379520ecdf94fbed358b91be7e0e','2009-04-08 12:44:41','2009-04-08 12:45:50',NULL,NULL,NULL,'2009-04-08 12:45:50','active',NULL);"
   end
 
   def self.down
--- db/schema.rb
+++ db/schema.rb
@@ -30,19 +30,20 @@ ActiveRecord::Schema.define(:version => 20090429142152) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "login",                     :limit => 40
-    t.string   "name",                      :limit => 100, :default => ""
-    t.string   "email",                     :limit => 100
-    t.string   "crypted_password",          :limit => 40
-    t.string   "salt",                      :limit => 40
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "remember_token",            :limit => 40
-    t.datetime "remember_token_expires_at"
-    t.string   "activation_code",           :limit => 40
-    t.datetime "activated_at"
-    t.string   "state",                                    :default => "passive"
-    t.datetime "deleted_at"
+    t.string   "login",                              :null => false
+    t.string   "email",                              :null => false
+    t.string   "crypted_password",                   :null => false
+    t.string   "password_salt",                      :null => false
+    t.string   "persistence_token",                  :null => false
+    t.string   "single_access_token",                :null => false
+    t.string   "perishable_token",                   :null => false
+    t.integer  "login_count",         :default => 0, :null => false
+    t.integer  "failed_login_count",  :default => 0, :null => false
+    t.datetime "last_request_at"
+    t.datetime "current_login_at"
+    t.datetime "last_login_at"
+    t.string   "current_login_ip"
+    t.string   "last_login_ip"
   end
 
   add_index "users", ["login"], :name => "index_users_on_login", :unique => true
