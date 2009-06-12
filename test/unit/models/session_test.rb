e031e64efcd8580d769e794fbd88a8adc475070a
jbeck <jbeck@461604b8-cbb9-4c6a-b984-f4fa5d6bc773>
2009-06-12 19:22:25 +0000

    Minor refactor.
    git-svn-id: svn+ssh://svn.beckproduct.com/svnroot/justinbeck/trunk@98 461604b8-cbb9-4c6a-b984-f4fa5d6bc773

--- test/unit/models/session_test.rb
+++ test/unit/models/session_test.rb
@@ -4,7 +4,7 @@ class SessionTest < ActiveSupport::TestCase
   setup :activate_authlogic
 
   def test_create_session
-    session = create_session(:login => "session_user", :password_salt => salt = Authlogic::Random.hex_token, :crypted_password => Authlogic::CryptoProviders::Sha512.encrypt("benrocks" + salt), :persistence_token => Authlogic::Random.hex_token)
+    session = create_session(:login => "session_user", :password_salt => salt = Authlogic::Random.hex_token, :crypted_password => Authlogic::CryptoProviders::Sha512.encrypt("justinrocks" + salt), :persistence_token => Authlogic::Random.hex_token)
     assert !session.new_record?, "#{session.errors.full_messages.to_sentence}"
   end
 
