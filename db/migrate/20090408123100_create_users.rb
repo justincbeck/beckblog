bef5002199e4c4d8084469a5267052197e129ceb
jbeck <jbeck@461604b8-cbb9-4c6a-b984-f4fa5d6bc773>
2009-06-12 04:12:53 +0000

    Added ME to the users migration so there is a user when the database is initialy migrated.
    git-svn-id: svn+ssh://svn.beckproduct.com/svnroot/justinbeck/trunk@93 461604b8-cbb9-4c6a-b984-f4fa5d6bc773

--- db/migrate/20090408123100_create_users.rb
+++ db/migrate/20090408123100_create_users.rb
@@ -17,6 +17,8 @@ class CreateUsers < ActiveRecord::Migration
     add_index :users, :login
     add_index :users, :persistence_token
     add_index :users, :last_request_at
+
+    execute "INSERT INTO `users` VALUES (1,'2009-06-12 04:00:15','2009-06-12 04:08:01','justin','0579d36836d78b6b022420af249e37726831e480a1b4210f54a50f8c0707e3ca6769b92a4ac35ec97680ff4d9c8f5678495bcfb584bf767999d0d0694e5fc451','zAVe15D-SV9oYkUzRK9a','0527fafe910dbeb7afe0d9fb1ec8822d0c65db4efa8477c9c41e7d8dd4fe8031eb9fc0d9e0a036b0d76159a537b8cf19323f03fe90fba614bebfc492cf0dc0e0',4,'2009-06-12 04:08:01','2009-06-12 04:04:33','2009-06-12 04:05:10','127.0.0.1','127.0.0.1');"
   end
 
   def self.down
