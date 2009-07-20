de35297084a4ff5acfafc5a8929dbbcf3131cc58
jbeck <justinbeck@mac.com>
2009-07-20 07:07:21 -0400

    Again, more work on the capistrano stuff.

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,11 +3,12 @@ set :application, "beckblog"
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-set :deploy_to, "/opt/myapp"
+set :deploy_to, "/opt/beckblog"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 set :scm, :git
+set :scm_command, "/opt/git-1.6.3.2/bin/git"
 set :repository, "git@github.com:justincbeck/beckblog.git"
 set :branch, "master"
 set :deploy_via, :remote_cache
@@ -15,7 +16,9 @@ set :deploy_via, :remote_cache
 set :user, 'jbeck'
 set :ssh_options, { :forward_agent => true }
 
-role :db,  "justinbeck.com", :primary => true
+ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa")]
+
+role :db,  "88.201.1.207", :primary => true
 
 namespace :deploy do
   desc "Restarting mod_rails with restart.txt"
